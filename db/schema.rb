ef978d10cc8359cc0d2abef38d5efbc7b5a4271c
Elizabeth Uselton <elizabeth.uselton@gmail.com>
2014-01-05 12:48:32 -0800

    Changed database to Postgres the right way

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140104203636) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "bookcase_books", force: true do |t|
     t.integer  "book_id"
     t.integer  "bookcase_id"
